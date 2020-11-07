9bf0ea7f6f6f02428630ee99ef8e3265fcaff59d
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-11-07 17:20:12 +0000

    "OK"

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 user=$1
 description=$2
 repo=$(basename `git rev-parse --show-toplevel`)
 latest_tag=$(git describe --abbrev=0)
 source name.txt
+cd bin/
 grel release -u $user -r $repo --tag $latest_tag --name "New release" --description '"'"$description"'"'
 grel upload -u $user -r $repo --tag $latest_tag --name "$grader_app_name.exe" --file $grader_app_name.exe
 grel upload -u $user -r $repo --tag $latest_tag --name "$grader_app_name.macos" --file $grader_app_name.mac
