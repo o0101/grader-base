d52ea56e318b33eb3b778b870e06dc2eb4526592
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-09-12 10:28:52 +0000

    Updates

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  name: "JSPaint",
+  name: "My App Name",
   entry: "./app.js",
   author: {
-    name: "Cris Stringfellow",
-    url: "https://github.com/i5ik",
+    name: "My Name",
+    url: "https://github.com/my-github",
   },
   desiredPort: 49666,
   version: "0.0.1",
-  description: "Classic JS Paint",
-  source: "https://github.com/i5ik/jspaint.exe",
+  description: "A cross-platform app",
+  source: "https://github.com/my-github/my-open-source",
   organization: {
-    name: "Dosyago",
-    url: "https://github.com/dosyago"
+    name: "My org",
+    url: "https://github.com/my-org"
   },
   apiOrigins: [],                   // exact origins allowed to call Service API via grader global,
   DEBUG: (process.env.GR_DEBUG && (process.env.GR_DEBUG === "false" ? false : true)) || false
